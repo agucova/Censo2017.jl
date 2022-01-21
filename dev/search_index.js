da9b10ae67cd884f8d8a790cc99fa8c2ee20ba69
Documenter.jl <documenter@juliadocs.github.io>
2022-01-21 19:54:19 +0000

    build based on e1eecf5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Censo2017","category":"page"},{"location":"#Censo2017","page":"Home","title":"Censo2017","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Censo2017.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Censo2017]","category":"page"}]
+}
